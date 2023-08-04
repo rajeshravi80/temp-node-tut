ea937e1534a754c9e1956a284a2721fc0b10f2b8
Rajesh Ravindran <rajesh.ravi@gmail.com>
2023-08-04 13:22:49 -0400

    second change

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
-const _ = require('lodash')
+const http = require('http');
 
-const items = [1,[2,[3,[4]]]]
-const newItems = _.flattenDeep(items)
-console.log(newItems);
-console.log("Hello World");
+const server = http.createServer((req,res)=>{
+
+    if(req.url ==='/'){
+        res.end('Home Page')
+        return
+    }
+    if(req.url ==='/about'){
+        //BLOCKING CODE
+        // for(let i=0; i<1000; i++){
+        //     for(let j=0; j<1000; j++){
+        //         console.log(`${i} ${j}`);
+        //     }
+        // }
+        res.end('About Page')
+        return
+    }
+    res.end('Error page')
+    return
+
+})
+
+server.listen(5000, ()=>{
+    console.log('Server is listening on port 5000...')
+})
